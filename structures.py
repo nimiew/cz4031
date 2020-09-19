40d332576c2006858bafb84f30022b6c2b1e8d46
Jun Liang Ang <jang047@e.ntu.edu.sg>
2020-09-19 15:28:36 +0800

    Update utils

--- structures.py
+++ structures.py
@@ -1,4 +1,11 @@
 import copy # Using deepcopy, to enforce using write methods by client
+from utils import *
+
+# Constants
+RECORD_SIZE = 18 # 10 + 4 + 4
+BLOCK_SIZE = 100
+DISK_SIZE = 100 * 1024 * 1024
+NUM_BLOCKS = DISK_SIZE // BLOCK_SIZE
 
 class Block:
     def __init__(self, block_size):
@@ -15,36 +22,30 @@ class Block:
 
 class Disk:
     # Use this class as a static class. Don't instantiate. All methods are class methods.
-
-    # Constants
-    RECORD_SIZE = 18 # 10 + 4 + 4
-    BLOCK_SIZE = 100
-    DISK_SIZE = 100 * 1024 * 1024
-    NUM_BLOCKS = DISK_SIZE // BLOCK_SIZE
     
-    # Disk variables
     blocks = []
     for _ in range(NUM_BLOCKS):
         blocks.append(Block(BLOCK_SIZE))
-    next_free_block = 0
+    next_free_block_id = 0
     
     @classmethod
     def read_block(cls, address):
-        if not 0 <= address < cls.NUM_BLOCKS:
-            raise Exception(f"Invalid block id. Address must be within [0, {cls.NUM_BLOCKS-1}]")   
+        if not 0 <= address < NUM_BLOCKS:
+            raise Exception(f"Invalid block id. Address must be within [0, {NUM_BLOCKS-1}]")   
         return copy.deepcopy(cls.blocks[address])
     
     @classmethod
     def write_block(cls, address, block):
-        if not 0 <= address < cls.NUM_BLOCKS:
-            raise Exception(f"Invalid block id. Address must be within [0, {cls.NUM_BLOCKS-1}]")
+        if not 0 <= address < NUM_BLOCKS:
+            raise Exception(f"Invalid block id. Address must be within [0, {NUM_BLOCKS-1}]")
         cls.blocks[address] = block
         
     @classmethod
     def write_free_block(cls, block):
         self.blocks[cls.next_free_block] = block
-        next_free_block += 1
-    
+        next_free_block += 1 
+
     @classmethod
     def info(cls):
-        return f"Disk size: {cls.DISK_SIZE}, Block size: {cls.BLOCK_SIZE}, No. blocks: {cls.NUM_BLOCKS}"
+        return f"Disk size: {DISK_SIZE}, Block size: {BLOCK_SIZE}, No. blocks: {NUM_BLOCKS}"
+        
--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 def parse_data(path="data.tsv"):
     with open("data.tsv") as f:
         lines = f.readlines()
-    return [line.split() for i, line in enumerate(lines) if i != 0]
+    data = [line.split() for i, line in enumerate(lines) if i != 0]
+    for record in data:
+        record[1] = float(record[1])
+        record[2] = int(record[2])
+    return data
 
 def convert_string_to_bytes(string, num_bytes):
-    # string => list[int]
-    # 0 <= int <= 255
+    # string => bytearray
     if len(string) > num_bytes:
         raise Exception(f"string length: {len(string)} exceeds num_bytes: {num_bytes}")
     res = []
@@ -15,10 +18,10 @@ def convert_string_to_bytes(string, num_bytes):
             raise Exception(f"byte value: {byte_value} invalid")
         res.append(byte_value)
     res.extend([0 for _ in range(num_bytes - len(res))])
-    return res
+    return bytearray(res)
 
 def convert_bytes_to_string(bytes_):
-    # bytearray or list[int] => string
+    # bytearray => string
     res = []
     for byte in bytes_:
         if byte == 0:
@@ -26,17 +29,25 @@ def convert_bytes_to_string(bytes_):
         res.append(chr(byte))
     return "".join(res)
 
+def convert_number_to_bytes(number):
+    # int => bytearray
+    return bytearray(number.to_bytes(4, byteorder='little', signed=False))
+
+def convert_bytes_to_number(bytes_):
+    # bytearray => int
+    return int.from_bytes(bytes_, byteorder='little', signed=False)
+
 def convert_float_to_bytes(real):
     # float => bytearray
     before_dot, after_dot = str(real).split(".")
     before_dot = int(before_dot).to_bytes(2, byteorder='little', signed=False)
     after_dot = int(after_dot).to_bytes(2, byteorder='little', signed=False)
-    return before_dot + after_dot # concat
+    return bytearray(before_dot + after_dot) # concat
 
-def convert_bytes_to_float(num_bytes):
-    # bytearray or list[int] => float
-    before_dot = str(int.from_bytes(num_bytes[:2], byteorder='little', signed=False))
-    after_dot = str(int.from_bytes(num_bytes[2:], byteorder='little', signed=False))
+def convert_bytes_to_float(bytes_):
+    # bytearray => float
+    before_dot = str(int.from_bytes(bytes_[:2], byteorder='little', signed=False))
+    after_dot = str(int.from_bytes(bytes_[2:], byteorder='little', signed=False))
     return float(before_dot + "." + after_dot)
 
 def is_index_block(block):
