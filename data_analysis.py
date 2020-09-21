e6db45110314b60f13e1c311315a046d27d5f9e3
Jun Liang Ang <jang047@e.ntu.edu.sg>
2020-09-22 02:18:18 +0800

    Fix

--- data_analysis.py
+++ data_analysis.py
@@ -11,10 +11,12 @@ def main():
     min_average_rating = float('inf')
     max_num_votes = float("-inf")
     min_num_votes = float("inf")
+    average_rating_unique = set()
 
     # loop through all the data rows
     for line in lines[1:]:
         tconst, average_rating, num_votes = line.split()
+        average_rating_unique.add(average_rating)
         max_tconst_len = max(max_tconst_len, len(tconst))
         max_average_rating = max(max_average_rating, float(average_rating))
         min_average_rating = min(min_average_rating, float(average_rating))
@@ -31,6 +33,7 @@ def main():
     print(f"Number of records: {len(lines[1:])}")
     print(f"Minimum average rating: {min_average_rating}")
     print(f"Maximum average rating: {max_average_rating}")
+    print(f"Unique average rating: {len(average_rating_unique)}")
     print(f"Minimum number of votes: {min_num_votes}")
     print(f"Maximum number of votes: {max_num_votes}")
     print(f"Maximum tconst length: {max_tconst_len}")
